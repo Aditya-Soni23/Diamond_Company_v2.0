129bd31316005a1f63f6f79411fdd2eb5f9fbb9a
Aditya-Soni23 <adityasonihyderabad@gmail.com>
2026-01-02 10:05:41 +0530

    backtotop

--- main.js
+++ main.js
@@ -106,3 +106,21 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+
+  const btn = document.getElementById("backToTop");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      btn.style.display = "block";
+    } else {
+      btn.style.display = "none";
+    }
+  });
+
+  btn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+
